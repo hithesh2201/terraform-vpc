1333511a53a44003b319b0606533fded8a6531a3
Hitesh <hithesh2201@gmail.com>
2024-02-22 11:02:25 +0530

    adding some other configurarions

--- main.tf
+++ main.tf
@@ -81,9 +81,15 @@ resource "aws_route" "public" {
   gateway_id             = aws_internet_gateway.gw.id
 }
 
-resource "aws_route_table_association" "public" {
+resource "aws_route_table_association" "public_0" {
 
   subnet_id      = aws_subnet.public[0].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_1" {
+
+  subnet_id      = aws_subnet.public[1].id
   route_table_id = aws_route_table.public.id
 }
 
@@ -119,9 +125,15 @@ resource "aws_route" "private" {
   gateway_id             = aws_nat_gateway.ngw.id
 }
 
-resource "aws_route_table_association" "private" {
+resource "aws_route_table_association" "private_0" {
 
   subnet_id      = aws_subnet.private[0].id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_1" {
+
+  subnet_id      = aws_subnet.private[1].id
   route_table_id = aws_route_table.private.id
 }
 
@@ -139,9 +151,14 @@ resource "aws_route" "database" {
   gateway_id             = aws_nat_gateway.ngw.id
 }
 
-resource "aws_route_table_association" "database" {
+resource "aws_route_table_association" "database_0" {
 
   subnet_id      = aws_subnet.database[0].id
+  route_table_id = aws_route_table.database.id
+}
+resource "aws_route_table_association" "database_1" {
+
+  subnet_id      = aws_subnet.database[1].id
   route_table_id = aws_route_table.database.id
 }
 resource "aws_db_subnet_group" "default" {
